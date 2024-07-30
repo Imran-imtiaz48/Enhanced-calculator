271d8db31a49588ac7668728c161a686c0be4040
Imran Imtiaz <imran.imtiaz@daralber.ae>
2024-07-30 09:46:13 +0400

    Calculator Source Code

---
+++ Calculator.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double num1, num2, result;
+
+    // Display menu
+    printf("Select operation:\n");
+    printf(" + for addition\n");
+    printf(" - for subtraction\n");
+    printf(" * for multiplication\n");
+    printf(" / for division\n");
+    printf("Enter operator: ");
+    scanf(" %c", &operator);
+
+    // Get numbers from user
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    // Perform calculation based on operator
+    switch(operator) {
+        case '+':
+            result = num1 + num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                printf("Result: %.2lf\n", result);
+            } else {
+                printf("Error! Division by zero.\n");
+            }
+            break;
+        default:
+            printf("Error! Invalid operator.\n");
+            break;
+    }
+
+    return 0;
+}
