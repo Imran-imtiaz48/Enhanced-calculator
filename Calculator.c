596d24c048e156404678989d4b1da54c68aa964a
Imran Imtiaz <imran.imtiaz@daralber.ae>
2024-07-30 09:48:53 +0400

    Added Functions in Source code

--- Calculator.c
+++ Calculator.c
@@ -1,47 +1,72 @@
 #include <stdio.h>
+#include <math.h>
 
-int main() {
-    char operator;
-    double num1, num2, result;
-
-    // Display menu
+void print_menu() {
     printf("Select operation:\n");
     printf(" + for addition\n");
     printf(" - for subtraction\n");
     printf(" * for multiplication\n");
     printf(" / for division\n");
-    printf("Enter operator: ");
-    scanf(" %c", &operator);
+    printf(" ^ for exponentiation (power)\n");
+    printf(" sqrt for square root\n");
+    printf(" Enter operation: ");
+}
 
-    // Get numbers from user
-    printf("Enter two numbers: ");
-    scanf("%lf %lf", &num1, &num2);
+int main() {
+    char operation[10];
+    double num1, num2, result;
 
-    // Perform calculation based on operator
-    switch(operator) {
-        case '+':
-            result = num1 + num2;
+    // Display menu
+    print_menu();
+    scanf("%s", operation);
+
+    if (operation[0] == 's' && operation[1] == 'q' && operation[2] == 'r' && operation[3] == 't') {
+        // Square root calculation
+        printf("Enter a number: ");
+        scanf("%lf", &num1);
+        if (num1 < 0) {
+            printf("Error! Negative number for square root.\n");
+        } else {
+            result = sqrt(num1);
             printf("Result: %.2lf\n", result);
-            break;
-        case '-':
-            result = num1 - num2;
-            printf("Result: %.2lf\n", result);
-            break;
-        case '*':
-            result = num1 * num2;
-            printf("Result: %.2lf\n", result);
-            break;
-        case '/':
-            if (num2 != 0) {
-                result = num1 / num2;
+        }
+    } else if (operation[0] == '^') {
+        // Exponentiation
+        printf("Enter base and exponent: ");
+        scanf("%lf %lf", &num1, &num2);
+        result = pow(num1, num2);
+        printf("Result: %.2lf\n", result);
+    } else {
+        // Get two numbers from user
+        printf("Enter two numbers: ");
+        scanf("%lf %lf", &num1, &num2);
+
+        // Perform calculation based on operator
+        switch (operation[0]) {
+            case '+':
+                result = num1 + num2;
                 printf("Result: %.2lf\n", result);
-            } else {
-                printf("Error! Division by zero.\n");
-            }
-            break;
-        default:
-            printf("Error! Invalid operator.\n");
-            break;
+                break;
+            case '-':
+                result = num1 - num2;
+                printf("Result: %.2lf\n", result);
+                break;
+            case '*':
+                result = num1 * num2;
+                printf("Result: %.2lf\n", result);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    printf("Result: %.2lf\n", result);
+                } else {
+                    printf("Error! Division by zero.\n");
+                }
+                break;
+            default:
+                printf("Error! Invalid operation.\n");
+                break;
+        }
     }
 
     return 0;
